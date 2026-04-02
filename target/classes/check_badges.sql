f82a5d9414fce3765ee1cc429c50d705681e6ab0
Harshit Singh <harshitsingh2807@gmail.com>
2026-04-02 21:03:06 +0530

    fix

--- target/classes/check_badges.sql
+++
@@ -1,5 +0,0 @@
--- Quick diagnostic: Check if badges exist
-SELECT COUNT(*) as total_badges FROM badges;
-
--- If 0, badges haven't been added yet
--- Run setup_badges_auto.sql first!
--- target/classes/db/migration/V12__fix_badge_constraints.sql
+++
@@ -1,5 +0,0 @@
--- Fix badge constraint to allow all criteria types
-ALTER TABLE badges DROP CONSTRAINT IF EXISTS badges_criteria_type_check;
-
-ALTER TABLE badges ADD CONSTRAINT badges_criteria_type_check 
-CHECK (criteria_type IN ('POINTS', 'STREAK', 'CONSISTENCY', 'DAYS_ACTIVE', 'MILESTONE'));
